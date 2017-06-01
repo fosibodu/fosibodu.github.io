b0894f7544bebfec9a959bd77fee9e9153c603e0
Folake Osibodu <folakeosibodu@gmail.com>
2017-06-01 18:30:04 -0500

    changes to portfolio. redone work page mostly and “cleaned up” a lot of code

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
-    $(".photo-list > h1:nth-child(1) > a").click(function () {
+    $(".photo-list > h3:nth-child(1)").click(function () {
         $(".event, .misc, .photoj, .portrait").hide();
         $(".sports").show();
     });
     
-    $(".photo-list > h1:nth-child(2) > a").click(function () {
+    $(".photo-list > h3:nth-child(2)").click(function () {
         $(".sports, .misc, .photoj, .portrait").hide();
         $(".event").show();
     });
     
-    $(".photo-list > h1:nth-child(3) > a").click(function () {
+    $(".photo-list > h3:nth-child(3)").click(function () {
         $(".sports, .misc, .photoj, .event").hide();
         $(".portrait").show();
     });
     
-    $(".photo-list > h1:nth-child(4) > a").click(function () {
+    $(".photo-list > h3:nth-child(4)").click(function () {
         $(".event, .misc, .sports, .portrait").hide();
         $(".photoj").show();
     });
     
-    $(".photo-list > h1:nth-child(5) > a").click(function () {
+    $(".photo-list > h3:nth-child(5)").click(function () {
         $(".event, .sports, .photoj, .portrait").hide();
         $(".misc").show();
     });
